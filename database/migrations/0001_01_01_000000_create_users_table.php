211208b19c3c33298dd2a74b11130d35f333ce89
khobabe <khobabe007@gmail.com>
2024-07-31 18:14:30 +0530

    firsst

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('contact')->unique();
+            $table->enum("gender",["male","female","other"]);
+            $table->string('education_qualification')->unique();
+            $table->boolean("isAdmin")->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ resources/views/public/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <h1 class="bg-red-500 text-white px-5 py-4 rounded ">hello india</h1>
+    <h2 class="bg-green-500 text-white px-5 py-4 rounded ">This is cws , a very good platform for techies</h2>
+</body>
+</html>
