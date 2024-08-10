582b47e441c9726e5ec00e57abdcc77316ec5246
khobabe <khobabe007@gmail.com>
2024-08-10 10:35:14 +0530

    admin controller created

--- app/Http/Controllers/PublicControlller.php
+++ app/Http/Controllers/PublicControlller.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class PublicControlller extends Controller
 {
-    //
+    public function index(){
+        return view("homepage");
+    }
+
+    public function logIn(){
+
+    }
+
+    public function apply(){
+
+    }
+
+    public function viewCourse(){
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\PublicControlller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', function(){
     return view('welcome');
 });
+
+// Route::get('/',function(){
+//     return view("public.layout");
+// });
