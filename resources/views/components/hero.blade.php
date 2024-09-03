2cc200241d757dfd87e43fe47f9cd6657a6afd72
sadique-cws <sadique.hussain96@gmail.com>
2024-09-03 11:12:36 +0530

    banner

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,14 +1,14 @@
 <div>
     <div class="relative flex flex-1 md:flex-row flex-col bg-slate-100 h-auto md:h-[500px] my-0 py-7 md:py-2 items-center px-5 md:px-[10%] overflow-hidden">
         <!-- Background Image and Gradient Overlay -->
-        <div class="absolute inset-0 bg-gradient-to-l z-10 from-slate-900 via-slate-800 to-black opacity-60"></div>
+        <div class="absolute inset-0 bg-gradient-to-r z-10 from-slate-100 via-slate-500 to-black opacity-50"></div>
         <div class="absolute inset-0 bg-cover bg-center" style="background-image: url('{{ asset('assets/banner.png') }}');"></div>
 
-        <div class="relative flex-1 flex-col gap-1 z-50">
-            <h1 class="md:text-5xl text-3xl font-semibold leading-normal text-white">
+        <div class="relative flex-1 flex-col max-w-lg ml-5 gap-1 z-50">
+            <h1 class="md:text-5xl text-3xl font-semibold leading-normal text-gray-900">
                 Purnea's <span class="text-orange-500">Most Trusted</span> Software Company
             </h1>
-            <p class="text-lg md:text-xl font-light text-gray-200 mt-2">
+            <p class="text-lg md:text-xl font-normal text-gray-900 mt-4">
                 Empowering you with the skills to achieve your dreams. Start your journey towards success today!
             </p>
             <br>
@@ -63,10 +63,10 @@
 
             <!-- Main Modal -->
             <div id="authentication-modal" class="hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50">
-                <div class="relative p-4 w-full max-w-xl max-h-full">
+                <div class="relative p-4 w-full max-w-sm max-h-full">
                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        <div class="flex items-center justify-between p-2 md:p-3 border-b rounded-t dark:border-gray-600">
+                            <h3 class="text-md font-semibold text-gray-900 dark:text-white">
                                 Enter your Required Details
                             </h3>
                             <button type="button" onclick="toggleModal()" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
@@ -76,12 +76,12 @@
                                 <span class="sr-only">Close modal</span>
                             </button>
                         </div>
-                        <div class="p-4 md:p-5">
-                            <form class="space-y-4" action="{{ route('public.hireUs') }}" method="post">
+                        <div class="p-2 md:p-3">
+                            <form class="space-y-2" action="{{ route('public.hireUs') }}" method="post">
                                 @csrf
                                 <div>
                                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
-                                    <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Ex: denish riche" required />
+                                    <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="E.g Rajesh Kumar" required />
 
                                     @error('name')
                                     <p class="text-red-500 text-xs font-semibold">{{ $message }}</p>
@@ -89,14 +89,15 @@
                                 </div>
                                 <div>
                                     <label for="contact" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contact</label>
-                                    <input type="number" name="contact" id="contact" placeholder="Ex: 9988776655" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                                    <input type="number" name="contact" id="contact" placeholder="E.g 99999-99999" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
 
                                     @error('contact')
                                     <p class="text-red-500 text-xs font-semibold">{{ $message }}</p>
                                     @enderror
                                 </div>
 
-                                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                                <button type="submit" class="w-full text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">Send Information</button>
+                                
                             </form>
                         </div>
                     </div>
