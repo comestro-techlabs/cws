d4eede20a82c391a36cb4e37c04171dac46539bf
roni836 <ronitsaha836@gmail.com>
2024-09-04 23:11:57 +0530

    soft dev, web- dev url fixed

--- routes/web.php
+++ routes/web.php
@@ -91,10 +91,10 @@ Route::post('save-course-payment', [PaymentController::class, 'saveCoursePayment
                 Route::get('/coaching', 'coachingPage')->name('public.services.coaching');
                 Route::get('/ecommerce', 'ecommercePage')->name('public.services.ecommerce');
                 Route::get('/seo-services', 'seoServices')->name('public.services.seo-services');
-                Route::get('/web-dev', 'webDevPage')->name('public.services.web-dev');
+                Route::get('/web-developlent', 'webDevPage')->name('public.services.web-dev');
                 Route::get('/mobile-app', 'mobileAppPage')->name('public.services.mobile-app');
                 Route::get('/web-design', 'webDesignPage')->name('public.services.web-design');
-                Route::get('/software-dev', 'softwareDev')->name('public.services.software-dev');
+                Route::get('/software-development', 'softwareDev')->name('public.services.software-dev');
                 Route::get('/native-app', 'nativeApp')->name('public.services.native-app');
                 Route::get('/inventory-solution', 'inventorySolution')->name('public.services.invent-sol');
                 Route::get('/services', 'servicePage')->name('public.services.services');
