208a45b721bc8978550f8b4ee390de728811aba1
khobabe <khobabe007@gmail.com>
2024-09-04 12:16:13 +0530

    minor issue solved in public header linking

--- resources/views/public/publicheader.blade.php
+++ resources/views/public/publicheader.blade.php
@@ -169,7 +169,7 @@
                                   </a>
                               </li>
                               <li>
-                                  <a href="{{route('public.ecommerce')}}"
+                                  <a href="{{route('public.services.ecommerce')}}"
                                       class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                       E-Commerce Website Development
                                   </a>
