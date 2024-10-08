395aaf8130f9a1a03889d145ffc99441a1fe359f
sadique-cws <sadique.hussain96@gmail.com>
2024-10-08 16:41:55 +0530

    font work

--- resources/views/public/publicheader.blade.php
+++ resources/views/public/publicheader.blade.php
@@ -1,9 +1,9 @@
-<div id="alert-2" class=" justify-center flex-1 flex items-center p-4 mb-4 text-white bg-red-800" role="alert">
+<div id="alert-2" class=" justify-center flex-1 flex items-center p-4 mb-4 text-white bg-green-700" role="alert">
     <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
     </svg>
     <span class="sr-only">Info</span>   
-    <div class="ms-3 text-sm font-medium">
+    <div class="ms-3  text-xs md:text-sm font-medium">
         Code with SadiQ Now become Comestro Techlabs!
     </div>
 
