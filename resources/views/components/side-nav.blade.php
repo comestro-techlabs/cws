647cde4aefd97069d50293f6ff0509ee04ffbf30
smi-riti <smriti.kumari5104@gmail.com>
2025-01-12 18:51:36 +0530

    exam side nav

--- resources/views/components/side-nav.blade.php
+++ resources/views/components/side-nav.blade.php
@@ -248,12 +248,12 @@
             <ul id="exam" class="hidden py-2 space-y-2">
                 <li>
                     <a href="{{ route('exam.create') }}"
-                        class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Manage
+                        class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Insert
                         Exam</a>
                 </li>
                 <li>
                     <a href="{{ route('exam.show') }}"
-                        class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Insert
+                        class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Manage
                         Exam
                     </a>
                 </li>
