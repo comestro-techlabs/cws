a1cf9f13b1348aff70c813fff4ae2024593685da
SaritaKumari24 <saritaakumari24@gmail.com>
2025-01-17 12:32:19 +0530

    small changes

--- resources/views/auth/verify-otp.blade.php
+++ resources/views/auth/verify-otp.blade.php
@@ -21,7 +21,7 @@ register
             </div>
 
             <div class="mb-4">
-                <button type="submit" class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                <button type="submit" class="w-full bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                     Verify OTP
                 </button>
             </div>
