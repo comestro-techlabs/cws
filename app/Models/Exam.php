8fcb61b03403a447f74c2768576e3412d498b7e9
SaritaKumari24 <saritaakumari24@gmail.com>
2025-01-22 19:02:25 +0530

    work on dashboard

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -4,15 +4,22 @@ namespace App\Models;
 
 use Dotenv\Repository\Adapter\GuardedWriter;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Exam extends Model
 {
     protected $guarded = [];
+    // public function courses(): BelongsToMany
+    // {
+    //     return $this->belongsToMany(Course::class, 'course_user', 'user_id', 'course_id')->withPivot('batch_id')
+    //     ->withTimestamps();
+    // }
 
     public function course()
     {
         return $this->belongsTo(Course::class);
     }
+   
 
     public function quizzes()
     {
