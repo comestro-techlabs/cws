503c099888804cbf27897527f12fd2f21c0a94d3
sadique-cws <sadique.hussain96@gmail.com>
2025-02-08 08:09:46 +0530

    404 and 500 page design

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -19,8 +19,9 @@
     <div class="flex md:flex-row flex-col p-10  items-center justify-center min-h-screen md:px-[10%]">
         <div class="flex-[1.5] flex flex-col gap-3">
             <img src="{{asset('assets/learnSyntax.png')}}" alt="" class="w-[80%] md:w-[40%]">
-            <h1 class="text-xl text-slate-500"><strong class="text-black">404</strong>. That's an error</h1>
-            <p class="text-md md:text-2xl">The Requested URL <strong class="font-semibold">/{{request()->path()}}</strong> was not found on this server. <span class="text-slate-500">Thats all you know</span></p>
+             <h1 class="text-xl text-slate-500"><strong class="text-black">500</strong>. That's an error</h1>
+            <p class=" text-2xl">The server encountered an error and could not complete your Request.</p>
+            <p class="text-md md:text-2xl">If the problem persists. please contact care@learnsyntax.com. and report your problem and don't forget to mention this error message & query that caused it. <span class="text-slate-500">Thats all you know</span></p>
         </div>
         <div class="flex-1">
             <img src="{{asset('assets/404.jpg')}}" alt="" class="">
