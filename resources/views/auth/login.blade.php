6b9d7cc256224c8e493b541f56085aef531b3afe
shaique <Shaique.9434@gmail.com>
2025-02-15 17:21:55 +0530

    added spinner to create user page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,21 +19,6 @@ Login
         <p class="text-sm text-gray-600 mb-4">An OTP has been sent to your email. Please enter it below:</p>
 
         <livewire:auth.otp-modal-form :email="session('email')"/>
-       
-
-        <!-- here we will write the livewire component -->
-        <!-- <form action="{{ route('verify.otp') }}" method="POST" class="space-y-4">
-            @csrf
-            <input type="hidden" name="email" id="otp_email_hidden" value="{{ session('email') }}">
-
-            <div>
-                <label for="otp" class="block text-gray-700 text-sm font-bold mb-2">OTP</label>
-                <input type="text" name="otp" id="otp" class="w-full bg-gray-50 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 border border-gray-200 rounded-lg py-2.5 px-4 transition duration-300" placeholder="Enter OTP" required>
-                @error('otp') <span class="text-red-500 text-xs mt-1">{{ $message }}</span> @enderror
-            </div>
-
-            <button type="submit" class="w-full bg-blue-600 text-white font-bold py-2.5 px-4 rounded-lg hover:bg-blue-700 transition duration-300">Verify OTP</button>
-        </form> -->
 
         <form id="resend-otp-form" action="{{ route('auth.resend-otp') }}" method="POST">
             @csrf
@@ -43,27 +28,7 @@ Login
         </form>
     </div>
 </div>
-<!-- <script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const sendOtpForm = document.getElementById("send-otp-form");
-        const sendOtpBtn = document.getElementById("send-otp-btn");
-        const sendOtpText = document.getElementById("send-otp-text");
-        const loadingSpinner = document.getElementById("loading-spinner");
-        const otpModal = document.getElementById('otp-modal');
-        const closeModalButton = document.getElementById('close-modal');
 
-        sendOtpForm.addEventListener("submit", function () {
-            sendOtpBtn.disabled = true;
-            sendOtpText.textContent = "Sending...";
-            loadingSpinner.classList.remove("hidden");
-        });
-
-
-        closeModalButton.addEventListener('click', function () {
-            otpModal.classList.add('hidden');
-        });
-    });
-</script> -->
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         const closeModalButton = document.getElementById('close-modal');
