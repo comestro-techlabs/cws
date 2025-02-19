ce60917ca4205ceed8afa1efe3f8dd39d6686daa
shaique <Shaique.9434@gmail.com>
2025-02-19 23:29:11 +0530

    added hero section component

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -20,41 +20,11 @@
     <nav id="navbar" class="fixed top-0 z-50 w-full bg-white transition-all duration-300">
         <livewire:public.header />
     </nav>
-    <div class="bg-gradient-to-r  to-purple-100 via-purple-300 from-purple-50">
-        <!-- Content Section -->
-        <div class="md:pt-[30px] ">
-            <!-- Check if current route is homepage -->
-            <!-- Hero Section with Gradient -->
-            <div class="relative flex items-center md:pt-12 pt-10">
-                <div class="lg:px-[10%] w-full px-3 pt-5 md:py-0 gap-10 flex flex-col md:flex-row items-center">
-                    <!-- Left Section: Text Content -->
-                    <div
-                        class="flex-1 pt-10 lg:py-10 lg:w-1/2  bg-cover bg-bottom lg:bg-none bg-[url('{{ asset('hero-bg.png') }}')] w-full">
-                        <div class="rounded-2xl px-3 pb-5">
-                            <h1
-                                class="text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-bold text-slate-800 tracking-tight">
-                                Learn Syntax. <br> Learn Programming
-                            </h1>
-                            <p class="mt-2 w-[60%] text-md md:p-0 lg:hidden">Transform your passion for coding into a successful career </p>
-                            <p class="hidden lg:flex mt-5 w-[60%] text-lg md:p-0 lg:w-[90%]"> Transform your passion for coding into a successful career at Learn Syntax, Purnea's
-                                most trusted programming center. Join us for hands-on learning, expert guidance,
-                                and
-                                real-world projects to unlock your true potential!
-                            </p>
-                            </span>
-                            </p>
-                        </div>
-                    </div>
 
-                    <div
-                        class="w-full py-5 lg:w-1/2 hidden lg:flex bg-cover h-[400px] bg-right-bottom bg-[url('{{ asset('hero-bg.png') }}')]">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <livewire:public.herosection/>
     {{ $slot }}
     <livewire:public.footer />
+    
     @livewireScripts
 </body>
 
