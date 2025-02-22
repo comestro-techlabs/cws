dd554e05e6c7114f2cfa83c68153b18531276fba
rahulxdev0 <rk854327@gmail.com>
2025-02-22 14:43:49 +0530

    making folder structure in v3

---
+++ app/Livewire/V3/Public/Component/Header.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\V3\Public\Component;
+
+use Livewire\Component;
+
+class Header extends Component
+{
+    public function render()
+    {
+        return view('livewire.v3.public.component.header');
+    }
+}
---
+++ app/Livewire/V3/Public/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\V3\Public;
+
+use Livewire\Component;
+
+class Home extends Component
+{
+    public function render()
+    {
+        return view('livewire.v3.public.home');
+    }
+}
