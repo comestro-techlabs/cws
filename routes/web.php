a232c1165005c1b2f5487265813759671fb21763
aditi-19112002 <aditikeshri21@gmail.com>
2025-02-25 23:09:13 +0530

    livewire certificate work done

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,7 @@ use App\Livewire\Admin\Workshops\CreateWorkshop;
 use App\Livewire\Admin\Workshops\ManageWorkshop;
 use App\Livewire\Admin\Course\ManageCourse;
 use App\Livewire\Admin\Course\ShowCourse;
+use App\Livewire\Admin\Certificate\CertificateEligibility;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
 use App\Livewire\Public\Contact\ContactPage;
@@ -292,7 +293,9 @@ Route::middleware([AdminMiddleware::class, 'auth'])->group(function () {
        //Message Route
        Route::get('/message',CreateMessage::class)->name('admin.message.create');
        Route::get('/message/manage', ManageMessage::class)->name('admin.message.index');
-
+        
+    //    certificate
+    Route::get('/certificate',CertificateEligibility::class)->name('admin.certificate');
         });
     });
 
