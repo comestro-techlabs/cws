52d02a5ec12b0270b1bd214c97afd9b6bb791701
saurav itachi <sauravkumar52778@gmail.com>
2025-03-01 17:14:58 +0530

    learn syntax header logo work on process

--- resources/views/components/admin-header.blade.php
+++ resources/views/components/admin-header.blade.php
@@ -1,11 +1,12 @@
-<nav
-    class="font-sans flex text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full rounded-full">
-    <div class="mb-2 sm:mb-0 flex flex-row items-center">
-        <button @click="sidebarOpen = !sidebarOpen" class="lg:hidden p-2 focus:outline-none">
-            <img class="h-8 w-8 self-center " src="{{asset('apple-touch-icon.png')}}" />
-        </button>
-        <div class="hidden md:block">
-            <img class="h-4 w-28 self-center " src="{{asset('assets/LearnSyntax.png')}}" />
+<nav class="font-sans flex text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full rounded-full">
+        <div class="mb-2 sm:mb-0 flex flex-row">
+            <!-- Hamburger button (only on small screens) -->
+            <button @click="sidebarOpen = !sidebarOpen" class=" p-2 focus:outline-none">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                </svg>
+            </button>
+        </div>
 
         </div>
     </div>
