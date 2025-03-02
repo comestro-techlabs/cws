d887bfa4cf8c10e8c4668e15d156d291b4c29a9a
puja-03 <pujakumari11th2003@gmail.com>
2025-03-02 15:33:52 +0530

    add design in homepage  Meet Our Achievers

--- app/Livewire/Public/Home.php
+++ app/Livewire/Public/Home.php
@@ -3,7 +3,6 @@
 namespace App\Livewire\Public;
 
 use App\Models\Course;
-use App\Models\Payment;
 use App\Models\PlacedStudent;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
@@ -14,36 +13,32 @@ class Home extends Component
 {
     public $courses, $placedStudents, $title;
 
-
     public function mount()
     {
-
         $this->courses = Course::where("published", true)->latest()->take(6)->get();
-        $this->placedStudents = Cache::remember('placed_students_active', 60, function () {
-            return PlacedStudent::where('status', 1)->get();
+        $this->placedStudents = Cache::remember('placed_students_active_homepage', 60, function () {
+            return PlacedStudent::where('status', 1)->latest()->take(6)->get(); // Limit to 6 students
         });
         $this->title = "";
     }
-   
 
     public function courseDetails($slug)
     {
         $user = auth()->user();
 
-        if (Auth::check()) {
-            if (!$user->is_active) {
-                return redirect()->back()->with('error', 'Your account is inactive. Please contact support.');
-            }
-        } 
+        if (Auth::check() && !$user->is_active) {
+            return redirect()->back()->with('error', 'Your account is inactive. Please contact support.');
+        }
+
         $this->redirect(route('v2.public.courseDetail', ['slug' => $slug]), navigate: true);
     }
-
-
-
 
     #[Layout('components.layouts.app')]
     public function render()
     {
-        return view('livewire.public.home');
+        return view('livewire.public.home', [
+            'placedStudents' => $this->placedStudents,
+            'courses' => $this->courses,
+        ]);
     }
 }
