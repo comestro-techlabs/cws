15eb178e4cb23e9dcdefa5d128d922ee1a7040c8
saurav itachi <sauravkumar52778@gmail.com>
2025-03-03 10:35:08 +0530

    student notification route name fixed

--- resources/views/livewire/student/dashboard/student-dashboard.blade.php
+++ resources/views/livewire/student/dashboard/student-dashboard.blade.php
@@ -140,7 +140,7 @@
                 @else
                     <div class="space-y-4">
                         @foreach($messages as $message)
-                            <a wire:navigate href="{{ route('v2.student.messages.show', $message->id) }}" class="flex items-center bg-gray-50 p-4 rounded-lg  hover:bg-gray-100 transition duration-300">
+                            <a wire:navigate href="{{ route('v2.student.message.view',$message) }}" class="flex items-center bg-gray-50 p-4 rounded-lg  hover:bg-gray-100 transition duration-300">
                                 <svg class="w-6 h-6 mr-4 {{ $message->type == 'info' ? 'text-blue-500' : ($message->type == 'warning' ? 'text-yellow-500' : 'text-red-500') }}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 22c2.21 0 4-1.79 4-4H8c0 2.21 1.79 4 4 4zm6-6V9c0-3.07-2.24-5.64-5.21-5.93A3.992 3.992 0 0012 3a3.992 3.992 0 00-5.79 0C8.24 3.36 6 5.93 6 9v7l-2 2v1h16v-1l-2-2z" />
                                 </svg>
