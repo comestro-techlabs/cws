c51324c308e4f77b22a5f9b1dbe32980c91ceca0
sadique-cws <sadique.hussain96@gmail.com>
2025-03-13 15:59:45 +0530

    refactor: remove unused base layout and update student layout with new font styles

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,13 +1,13 @@
 <div class="min-h-screen flex items-center justify-center">
-    <div class="max-w-sm w-full px-4">
-        <div class="bg-white border rounded-lg shadow-sm p-6">
+    <div class="max-w-xl w-full px-4">
+        <div class="bg-white border rounded-lg shadow-sm p-6 h-[500px]">
             <div class="text-center mb-6">
-                <h1 class="text-3xl font-bold text-purple-600 mb-1">Login & Signup Here</h1>
+                <h1 class="text-3xl font-semibold text-gray-600 mb-1">Login & Signup Here</h1>
 
             </div>
-
+            <hr/>
             <!-- Google Sign In -->
-            <div class="flex flex-1 space-y-2 flex-col">
+            <div class="flex flex-1 space-y-2 flex-col mt-10">
                 @livewire('auth.google-login')
                 @livewire('auth.github')
             </div>
