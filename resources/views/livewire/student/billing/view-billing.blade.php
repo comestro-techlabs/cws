f32d18f1a3d508bf2a07f61ac5a5140c1b804ba7
saurav itachi <sauravkumar52778@gmail.com>
2025-03-14 22:54:15 +0530

    Add membership tab visibility based on user authentication status

--- resources/views/livewire/student/billing/view-billing.blade.php
+++ resources/views/livewire/student/billing/view-billing.blade.php
@@ -7,9 +7,14 @@
                     <button @click="activeTab = 'invoice'" :class="{ 'border-purple-500 text-purple-600': activeTab === 'invoice', 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300': activeTab !== 'invoice' }" class="w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm">
                         Invoice History
                     </button>
+                    @if (auth()->check() && auth()->user()->is_member)
+                
                     <button @click="activeTab = 'membership'" :class="{ 'border-purple-500 text-purple-600': activeTab === 'membership', 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300': activeTab !== 'membership' }" class="w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm">
                         Membership
                     </button>
+                    @else
+                    
+                    @endif
                 </nav>
             </div>
         </div>
