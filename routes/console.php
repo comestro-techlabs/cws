aaf362752da360c0042f3eda61b251681c9c2aed
shaique <Shaique.9434@gmail.com>
2025-03-16 15:49:43 +0530

    Implement exam notification system with scheduled reminders and job dispatching

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('exam:reminder')->everyMinute(); // Runs every day at 9 AM
