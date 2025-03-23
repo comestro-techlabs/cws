c57bb43128cf44a4601a25beec2fce2c41782a44
sadique-cws <sadique.hussain96@gmail.com>
2025-03-23 14:20:08 +0530

    menu btn remove

--- resources/views/components/student-navbar.blade.php
+++ resources/views/components/student-navbar.blade.php
@@ -1,12 +1,7 @@
 @props(['hasCompleted' => false, 'main' => ''])
 
 <div x-data="{ sidebarOpen: false }">
-    <button @click="sidebarOpen = !sidebarOpen" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-700 rounded-lg sm:hidden hover:bg-purple-50 focus:outline-none focus:ring-2 focus:ring-purple-200">
-        <span class="sr-only">Open sidebar</span>
-        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-        </svg>
-    </button>
+
     <aside id="default-sidebar"
         class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         :class="{'translate-x-0': sidebarOpen, '-translate-x-full': !sidebarOpen}">
