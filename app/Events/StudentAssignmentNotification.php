a55f21ad4012a8bfba088b62c509c60b0ac79b11
sadique-cws <sadique.hussain96@gmail.com>
2025-05-08 12:29:02 +0000

    change StudentAssignmentNotification and UserRegister classes to implement ShouldBroadcastNow for immediate broadcasting

--- app/Events/StudentAssignmentNotification.php
+++ app/Events/StudentAssignmentNotification.php
@@ -13,7 +13,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class StudentAssignmentNotification implements ShouldBroadcast
+class StudentAssignmentNotification implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
